2b1be020a98606d860fff42792e957b43cac08bf
jonathan <70296489+jsmith128@users.noreply.github.com>
2025-02-22 22:33:28 -0500

    Update for 2025
    Also made much more modular so it can be easy to modify for future years.

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-$(".hover-container").on("mousemove", function(e) {
-    //// box around cursor ////
-    var width = 27.5; // width of square in inches
+// image is 888in  x 360in
+// image is 2663px x 1079px
+// image x to field x=0: 99.125in
+// image y to field y=0: 21.5
 
-    var size = width / 0.4625;
+// 2663 / 888 = 2.998
+// 1079 / 360 = 2.997
+
+$(".hover-container").on("mousemove", function(e) {
+    var imageWidthPx = 2663;   
+    var imageHeightPx = 1079;
+
+    var imageWidthIn = 888;
+    var imageHeightIn = 360; 
+
+    // multiplier for inches to desired end units
+    var inchToUnits = 0.0254; // CURRENTLY METERS
+
+
+    //// CONVERSION FACTORS ////
+
+    // given unit * (desired unit / given unit)
+
+    // multiplier to convert from pixels to inches
+    var in2pxRatio = imageWidthPx / imageWidthIn 
+    var px2inRatio = imageWidthIn / imageWidthPx
+
+    // Offset from edge of image to y=x=0 on coordinate system
+    var xZeroOffset = 99.125 * in2pxRatio;
+    var yZeroOffset = 21.5 * in2pxRatio;
+
+    // Coordinate system with center at 0,0
+    if (false) {
+        xZeroOffset = imageWidthPx / 2;
+        yZeroOffset = imageHeightPx / 2;
+    }
+
+    //// BOX AROUND CURSOR ////
+    // Find the robot's width in pixels
+    var robotWidth = 27.5; // width of square in inches
+    robotWidth *= in2pxRatio;
+
     var parentOffset = $(this).parent().offset();
     //or $(this).offset(); if you really just want the current element's offset
     var relX = e.pageX - parentOffset.left;
@@ -10,10 +47,11 @@ $(".hover-container").on("mousemove", function(e) {
     var picHeight = $('.hover-image').height();
     var picWidth = $('.hover-image').height();
     $('.hover-image')
-    .css("left", relX - (size/2) + "px")
-    .css("top", relY - (size/2) + "px")
+    .css("left", relX - (robotWidth/2) + "px")
+    .css("top", relY - (robotWidth/2) + "px")
+    
 
-    //// coordinates ////
+    //// COORDINATES ////
 
     // var x = event.pageX - this.offsetLeft;
     // var y = event.pageY - this.offsetTop;
@@ -21,20 +59,30 @@ $(".hover-container").on("mousemove", function(e) {
     var x = event.pageX - parentOffset.left;
     var y = event.pageY - parentOffset.top;
 
-    x -= 960; // make center the origin
-    y -= 388.5;
-    
-    x *= 0.4625; // multiplier for pixel to inch conversion
-    y *= 0.4625;
+    // Offset (0,0)
+    x -= xZeroOffset;
+    y += yZeroOffset - imageHeightPx; // start from bottom
 
     y *= -1; // invert y
+    
+    // Convert from our image's pixels to real world units
+    var xInch = x * px2inRatio; 
+    var yInch = y * px2inRatio;
 
-    x = Math.round(x * 100) / 100 // round to 2 decimal places
-    y = Math.round(y * 100) / 100
+
+    var xUnit = Math.round(xInch * inchToUnits * 100) / 100;
+    var yUnit = Math.round(yInch * inchToUnits * 100) / 100;
+
+    xInch = Math.round(xInch * 100) / 100; // round to 2 decimal places
+    yInch = Math.round(yInch * 100) / 100;
 
 
-    document.getElementById("X").value="X: ".concat(x);
-    document.getElementById("Y").value="Y: ".concat(y);
+
+
+    document.getElementById("X").value="X: ".concat(xInch);
+    document.getElementById("Y").value="Y: ".concat(yInch);
+    document.getElementById("X2").value="X2: ".concat(xUnit);
+    document.getElementById("Y2").value="Y2: ".concat(yUnit);
 
     // alert("X Coordinate: " + x + " Y Coordinate: " + y);
 });
