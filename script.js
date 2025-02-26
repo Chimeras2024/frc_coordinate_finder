830e53bdcbfa0e375b6642a05589d5e2b3561f06
jonathan <70296489+jsmith128@users.noreply.github.com>
2025-02-26 17:05:51 -0500

    Add point generating functions
    Press P to add a point

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ $(".hover-container").on("mousemove", function(e) {
 
     //// BOX AROUND CURSOR ////
     // Find the robot's width in pixels
-    var robotWidthInch = 27.5; // width of square in inches
+    var robotWidthInch = 32; // width of square in inches
     robotWidth = robotWidthInch * in2pxRatio;
 
     var parentOffset = $(this).parent().offset();
@@ -98,3 +98,82 @@ $(".hover-container").on("mousemove", function(e) {
 
     // alert("X Coordinate: " + x + " Y Coordinate: " + y);
 });
+
+function promptPoint(){
+    var xy=prompt("X and Y coordinate in meters","0");
+    xy = xy.split(" ");
+    var x = parseFloat(xy[0]);
+    var y = parseFloat(xy[1]);
+    if (x!=null && y!=null) {
+        makeDot(x* 1/inchToUnits, y* 1/inchToUnits, 15);    
+    } else {
+        alert("error")
+    }
+}
+
+window.addEventListener('keydown', function(event) {
+    if (event.key == "p") {
+        promptPoint()
+    }
+});
+
+
+// makeDot(300,300, 30);
+// makeDot(0,0, 30);
+
+var pointsx = [];
+pointsx[0] = 3.14;
+pointsx[1] = 3.155;
+pointsx[2] = 3.775;
+pointsx[3] = 4.04;
+pointsx[4] = 5.185;
+pointsx[5] = 5.438;
+pointsx[6] = 5.96;
+pointsx[7] = 5.945;
+pointsx[8] = 5.324;
+pointsx[9] = 5.05;
+pointsx[10] = 3.914;
+pointsx[11] = 3.66;
+// pointsx[12] = 17.55/2;
+
+var pointsy = [];
+pointsy[0] = 4.13;
+pointsy[1] = 3.83;
+pointsy[2] = 2.86;
+pointsy[3] = 2.73;
+pointsy[4] = 2.78;
+pointsy[5] = 2.95;
+pointsy[6] = 3.97;
+pointsy[7] = 4.27;
+pointsy[8] = 5.23;
+pointsy[9] = 5.368;
+pointsy[10] = 5.311;
+pointsy[11] = 5.148;
+// pointsy[12] = 8.05/2;
+
+// Display points around red reef
+// for (let i = 0; i < pointsx.length; i++) {
+//     makeDot(pointsx[i] * 1/inchToUnits, pointsy[i] * 1/inchToUnits, 15);
+// }
+
+
+// Create a dot on the map, coordinates in inches.
+function makeDot(x, y, w) {
+    x *= in2pxRatio;
+    y *= in2pxRatio;
+
+    x += xZeroOffset;
+    y += yZeroOffset;
+
+    const dot = document.createElement('div');
+    dot.style.width = w+'px';
+    dot.style.height = w+'px';
+    dot.style.backgroundColor = 'white';
+    dot.style.borderRadius = '50%';
+    dot.style.position = 'absolute';
+    dot.style.left = x - (w/2) + 'px';
+    dot.style.top = document.documentElement.scrollHeight - y - (w) + 'px';
+    // dot.style.transform = 'translate(-50%, -50%)';
+    document.body.appendChild(dot);
+    return dot;
+}
